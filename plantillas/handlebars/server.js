fc2914bb3de7c2b5717785af43a0b275bde306a1
Matias Souto <masouto94@gmail.com>
2022-09-16 22:48:31 -0300

    Entrega de plantillas

--- plantillas/handlebars/server.js
+++ plantillas/handlebars/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const hbs = require('express-handlebars')
 const app = express()
+const products = []
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -16,28 +17,36 @@ app.engine('hbs',hbs.engine({
 app.set('views', './views')
 app.set('view engine', 'hbs')
 
-app.get('/', (req, res) => {
+app.get('/', (req,res) => {
+    res.redirect('/index')
+})
+
+app.get('/index', (req, res) => {
     res.render('index', {
-        titulo_1: "Ahora si",
-        titulo_2: "Esto se renderiza bien",
-        nombre: "El",
-        apellido: "mati",
-        linkTo: "/after"
+        linkTo: "products"
     })
 })
 
-app.get('/after', (req, res) => {
-    res.render('content', {
-        layout: "nextLayout",
-        title: "Sopa de macaco",
-        price: 245,
-        thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrnrDwoPWnRHMiRd3S_1WBRhQZWpUd95nf_w&usqp=CAU",
-        linkTo: "/",
-        showImage:true
+app.get('/products', (req, res) => {
+
+    res.render('productsTable', {
+        notEmpty: products.length > 0,
+        layout: "productsView",
+        productList: products,
+        linkTo: "index"
+    })
+})
+app.post('/products',(req,res)=>{
+    const {body} = req
+    products.push(body)
+
+    res.render('index', {
+        linkTo: "products",
+        success: true
     })
 })
 
-const PORT = process.env.PORT || 8081
+const PORT = process.env.PORT || 8082
 
 
 app.listen(PORT, () => {
