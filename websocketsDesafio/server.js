c7a89a9fd98b1deb38c7b9cdf1f2b3f904fe6655
Matias Souto <masouto94@gmail.com>
2022-09-27 16:52:50 -0300

    Wntrega websockets

--- websocketsDesafio/server.js
+++ websocketsDesafio/server.js
@@ -1,28 +1,74 @@
 const express = require('express')
+const hbs = require('express-handlebars')
 const http = require('http')
 const {Server: SocketServer} = require('socket.io')
 const app = express()
 const httpServer = http.createServer(app)
 const socketServer = new SocketServer(httpServer)
-const PORT = process.env.PORT || 3000
 
-app.use(express.static('./public'))
+const products = []
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname+'/views'))
+
+app.engine('hbs',hbs.engine({
+    partialsDir: __dirname+'/views/partials',
+    layoutsDir: __dirname+'/views/layouts',
+    extname:'.hbs',
+    defaultLayout:'default.hbs'
+    }))
+
+app.set('views', './views')
+app.set('view engine', 'hbs')
+
 app.get('/', (req,res) => {
-    res.sendFile('index.html')
+    res.redirect('/index')
 })
 
-const mensajes = []
+app.get('/index', (req, res) => {
+    res.render('index', {
+        linkTo: "products",
+        notEmpty: products.length > 0,
+        productList: products
+
+    })
+})
+
+app.get('/products', (req, res) => {
+
+    res.render('productsTable', {
+        notEmpty: products.length > 0,
+        layout: "productsView",
+        productList: products,
+        linkTo: "index"
+    })
+})
+app.post('/products',(req,res)=>{
+    const {body} = req
+    products.push(body)
+
+    res.render('index', {
+        linkTo: "products",
+        success: true
+    })
+})
+
+
+
 socketServer.on('connection', (client)=>{
     console.log(`Usuario ${client.id} conectado al socket`)
     
-    client.emit('handshake', mensajes)
+    socketServer.sockets.emit('showAllProds', products)
     
-    
-    client.on('mensaje', mensaje=>{
-        const obj = {id: client.id, user: mensaje.user, message:mensaje.message }
-        mensajes.push(obj)
-        socketServer.sockets.emit('mensajesArray', mensajes)
-        client.emit('userSelected')
+    client.on('productLoaded', product=>{
+        const obj = {
+            title: product.title,
+            price: product.price,
+            thumbnail: product.thumbnail
+        }
+        products.push(obj)
+        socketServer.sockets.emit('showAllProds', products)
     })
     
     client.on('disconnect', () => {
@@ -30,6 +76,14 @@ socketServer.on('connection', (client)=>{
     })
 })
 
+const PORT = process.env.PORT || 3000
+
 httpServer.listen(PORT, ()=>{
     console.log(`Server levantado en ${PORT}`)
 })
+
+
+
+// app.listen(PORT, () => {
+//     console.log(`Escuchando al puerto ${PORT} - plantillas`)
+// })
