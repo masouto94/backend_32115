78c6f58c950e963dd0e9461b897ba24970090cab
Matias Souto <masouto94@gmail.com>
2023-07-27 17:44:54 -0300

    format

--- 2023/desafio_2/ProductManagerFiles.js
+++ 2023/desafio_2/ProductManagerFiles.js
@@ -91,27 +91,27 @@ class ProductManager {
         return productList
     }
     getProducts = async () => {
-       return await fs.promises.readFile(this.path, "utf-8")
+        return await fs.promises.readFile(this.path, "utf-8")
             .then(elem => JSON.parse(elem))
             .then(res => res.map(item => {
                 let { id, ...rest } = item
                 let product = new Product(...Object.values(rest))
                 product.id = id
                 return product
-                }
+            }
             )
-        )
+            )
     }
 
     getProductById = async (id) => {
-        const found = await this.getProducts().then((res)=>res.find(item => item.id == id))
-        
+        const found = await this.getProducts().then((res) => res.find(item => item.id == id))
+
         if (!found) {
             throw new ProductNotFoundError(`Product with id: ${id} not found in Manager`)
         }
         return found
     }
-    getProductByCode = async (prods,code) => {
+    getProductByCode = async (prods, code) => {
         return prods.find(item => item.code == code)
     }
     addProduct = async (product) => {
@@ -119,9 +119,9 @@ class ProductManager {
         if (!(product instanceof Product)) {
             throw new TypeError("Can only add Product-like objects")
         }
-        const products = await this.getProducts() 
+        const products = await this.getProducts()
         //If no products in file override
-        if(products.length === 0){            
+        if (products.length === 0) {
             product.id = this.uid
             this.products.push(product)
             await this.saveToFile()
@@ -141,59 +141,59 @@ class ProductManager {
         return
     }
 
-    updateProduct = async (id, ...values) =>{
-        const products = await this.getProducts() 
-        this.products = await products.map((product) =>{
-             if(product.id === id){
-                Object.entries(...values).forEach( ([key,value]) =>{
+    updateProduct = async (id, ...values) => {
+        const products = await this.getProducts()
+        this.products = await products.map((product) => {
+            if (product.id === id) {
+                Object.entries(...values).forEach(([key, value]) => {
                     product[key] = value
                 })
             }
             return product
-            })
+        })
         await this.saveToFile()
         return this.products
     }
     deleteProduct = async (id) => {
-        const products = await this.getProducts() 
+        const products = await this.getProducts()
         this.products = products.filter(prod => prod.id !== id)
         this.saveToFile()
         return this.products
     }
     saveToFile = async () => {
-        await fs.promises.writeFile(this.path, JSON.stringify(this.products,null,"\t"))
+        await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, "\t"))
             .then(res => console.log(`File saved in ${this.path}`))
             .catch(rej => console.log(`Failed to save file in ${this.path}`))
     }
 }
 
 //Products
-const mate = new Product("Mate",10,100,10,"Un mate normal", "photoUrl")
-const yerba = new Product("Yerba",0,60,1000,"Yerba", "photoUrl")
-const vela = new Product("Vela",1,10,100,"Una vela para el corte de luz", "photoUrl")
-const televisor = new Product("Televisor",2,10,120000,"Televisor 4K", "photoUrl")
-const prods = [mate,yerba,vela,televisor]
+const mate = new Product("Mate", 10, 100, 10, "Un mate normal", "photoUrl")
+const yerba = new Product("Yerba", 0, 60, 1000, "Yerba", "photoUrl")
+const vela = new Product("Vela", 1, 10, 100, "Una vela para el corte de luz", "photoUrl")
+const televisor = new Product("Televisor", 2, 10, 120000, "Televisor 4K", "photoUrl")
+const prods = [mate, yerba, vela, televisor]
 
 //Product Manager
 const productManager = new ProductManager("/home/matias/Documents/misRepos/backend_32115/2023/desafio_2/products.js");
 
-(async()=>{
-    try{
-    await productManager.getProducts().then(r=>console.log(r))
+(async () => {
+    try {
+        await productManager.getProducts().then(r => console.log(r))
 
-    for await (let prod of prods){
-        await productManager.addProduct(prod)
+        for await (let prod of prods) {
+            await productManager.addProduct(prod)
+        }
+
+        await productManager.getProducts().then(r => console.log(r))
+        await productManager.updateProduct(3, { description: "La tele para ver al campeon" }).then(r => console.log(r))
+        await productManager.deleteProduct(1).then(r => {
+            console.log(r)
+            console.log("Se acabó la yerba")
+        })
     }
-
-    await productManager.getProducts().then(r=>console.log(r))
-    await productManager.updateProduct(3,{description: "La tele para ver al campeon"}).then(r=>console.log(r))
-    await productManager.deleteProduct(1).then(r=>{    
-    console.log(r)
-    console.log("Se acabó la yerba")
-    })
-}
-catch (e){
-    console.log(e)
-}
+    catch (e) {
+        console.log(e)
+    }
 
 })();
