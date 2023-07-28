7d2b2863fdb6643c50b943c4f22154d510f94a1a
Matias Souto <masouto94@gmail.com>
2023-07-28 14:48:49 -0300

    Added success status

--- 2023/desafio_3/NodeServer.js
+++ 2023/desafio_3/NodeServer.js
@@ -12,17 +12,17 @@ const app = express()
 app.use(express.urlencoded({extended:true}))
 
 app.get('/',(req, res) =>{
-    res.sendFile("views/availableRoutes.html",{root: __dirname })
+    res.status(200).sendFile("views/availableRoutes.html",{root: __dirname })
 })
 
 app.get('/products', async (req, res) =>{
     if(req.query.limit){
         const prods = await productManager.getProducts().then(r => r.slice(0,req.query.limit))
-        return res.send(prods)
+        return res.status(200).send(prods)
         
     }
     const prods = await productManager.getProducts()
-    return res.send(prods)
+    return res.status(200).send(prods)
 })
 
 app.get('/products/:id', async (req, res) =>{
@@ -30,7 +30,7 @@ app.get('/products/:id', async (req, res) =>{
     if(prods.length === 0){
         return res.status(404).send({error: "Product not found", description: `ID:${req.params.id} does not exist`})
     }
-    return res.send(prods)
+    return res.status(200).send(prods)
 })
 
 app.post('/products', async (req, res) =>{
@@ -38,7 +38,7 @@ app.post('/products', async (req, res) =>{
     if(prods.length === 0){
         return res.status(404).send({error: "Product not found", description: `ID:${req.body.productId} does not exist`})
     }
-    return res.send(prods)
+    return res.status(200).send(prods)
 })
 
 app.listen(8080,()=>{
