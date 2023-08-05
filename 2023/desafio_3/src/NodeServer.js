bdf47858cccedd0f9f6ecdb25780e5643e9e2da1
Matias Souto <masouto94@gmail.com>
2023-08-05 13:27:32 -0300

    added port

--- 2023/desafio_3/src/NodeServer.js
+++ 2023/desafio_3/src/NodeServer.js
@@ -6,6 +6,7 @@ import productsRouter from './routes/products.router.js'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const PORT = process.env.PORT || 8080
 
 const app = express()
 app.use(express.urlencoded({extended:true}))
@@ -16,7 +17,6 @@ app.get('/',(req, res) =>{
     res.status(200).sendFile("public/views/index.html",{root: __dirname })
 })
 
-
-app.listen(8080,()=>{
-    console.log("Connected to port 8080")
+app.listen(PORT,()=>{
+    console.log(`Connected to port ${PORT}`)
 })
