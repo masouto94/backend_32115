a2ca410fbb2c780edbdece808591b5e0b6221ddf
Matias Souto <masouto94@gmail.com>
2023-08-08 18:15:30 -0300

    WIP cart manager

---
+++ 2023/proyecto_final/src/model/Cart.js
@@ -0,0 +1,39 @@
+import { Product } from "./Product.js"
+
+class Cart {
+    constructor(products, id=undefined) {
+        this.products = products
+        this.price = this.products.reduce((accumulator, product) => {return accumulator + (product.price * product.quantity)}, 0)
+        this.#validateAttributes()
+        this.id = id ? id : Cart.generateId()
+
+    }
+#validateAttributes() {
+    // for (let product of this.products) {
+    //     if (!(product instanceof Product)) {
+    //         throw new TypeError("Products attribute must be composed of Product-like objects")
+    //     }
+    // }
+    return 
+}
+    static generateId(){
+        if(!this._id){
+            this._id = 1
+        }
+        const id = this._id
+        this._id++
+        return id
+        
+    }
+
+    static setBaseId(id){
+        this._id = id
+        return   
+    }
+
+}
+
+
+export {
+    Cart
+}
--- 2023/proyecto_final/src/model/CartManager.js
+++ 2023/proyecto_final/src/model/CartManager.js
@@ -1,7 +1,10 @@
 import fs from 'fs'
+import { Cart } from './Cart.js'
+import { Product } from './Product.js'
 
 class CartManager {
     #carts = []
+    #currentProducts = []
     constructor(path) {
         this.path = path
     }
@@ -12,14 +15,73 @@ class CartManager {
         this.#carts = value
     }
 
+    get currentProducts() {
+        return this.#currentProducts
+    }
+    set currentProducts(value){
+        this.#currentProducts = value
+    }
+
     getCarts = async () =>{
-        this.carts = await fs.promises.readFile(this.path).then(r => JSON.parse(r))
+        this.carts = await fs.promises.readFile(this.path)
+            .then(r => JSON.parse(r))
+            .then(res => res.map(item => {
+                const cart = new Cart(...Object.values(item))
+                return cart
+                })
+            )
         return this.carts
     }
+    addProduct = (product) => {
+        if(this.currentProducts.length === 0){
+            this.currentProducts.push({
+                ...product,
+                quantity: 1
+            })
+        }
+        this.currentProducts.map(prod => {
+            if (prod.code !== product.code){
+                return {
+                    ...product,
+                    quantity: 1
+                }
+            }else{
+                return {
+                    ...prod,
+                    quantity: prod.quantity++
+                }
+            }
+        })
+
+    }
+    createCart = (products) =>{
+        for(let product of products){
+
+        }
+        const cartToAdd = new Cart(this.currentProducts)
+        this.carts.push(cartToAdd)
+    }
+
+    saveCarts = async () =>{
+        await fs.promises.writeFile(this.path, JSON.stringify(this.carts, null, "\t"))
+    }
+
+
 
 
 }
 
+const mate = new Product("Mate", 0, 100, 10, "Un mate normal", "photoUrl")
+const yerba = new Product("Yerba", 1, 60, 1000, "Yerba", "photoUrl")
+
+const cartManager = new CartManager("/home/matias/Documents/misRepos/backend_32115/2023/proyecto_final/src/database/carts.json")
+
+// console.log(await cartManager.getCarts())
+// cartManager.createCart([mate,yerba])
+// await cartManager.saveCarts()
+cartManager.addProduct(mate)
+cartManager.addProduct(mate)
+console.log(cartManager.currentProducts)
 export {
     CartManager
 }
