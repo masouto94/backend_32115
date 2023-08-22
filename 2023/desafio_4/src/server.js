417ecd117018db4cac07c59ed9666fa7f720e10e
Matias Souto <masouto94@gmail.com>
2023-08-22 13:11:04 -0300

    desafio_4 as template; running websocket in proyectoFinal

--- 2023/desafio_4/src/server.js
+++ 2023/desafio_4/src/server.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import * as utils from './utils/utils.js'
 import {morgan, handlebars} from './utils/middlewares.js'
-import {Server} from 'socket.io'
 import { router } from './routes/views.router.js'
 
 const app = express()
@@ -16,17 +15,10 @@ app.use(express.static(utils.__public))
 
 app.use('/', router)
 
+
 const httpServer = app.listen(utils.PORT,()=>{
     console.log(`Connected to port ${utils.PORT} - HTTP`)
 })
-const socketServer = new Server(httpServer)
-socketServer.on('connection', (conn) =>{
-    conn.on('greeting', (message) => {
-        console.log(message.toUpperCase())
-    })
-    conn.on('inputMessage',(message,target)=>{
-        utils.renderMessage(message,target)
-    })
-})
+utils.socketServer(httpServer,utils.handlers)
 
 //Hay que pasar esto al proyecto final y hacer una pagina nueva
--- 2023/desafio_4/src/utils/utils.js
+++ 2023/desafio_4/src/utils/utils.js
@@ -1,6 +1,7 @@
 
 import path from 'path'
 import {fileURLToPath} from 'url'
+import { Server } from 'socket.io'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -8,6 +9,12 @@ const __views = `${__dirname}/../views`
 const __public = `${__dirname}/../public`
 const PORT = process.env.PORT || 8080
 
+const greeting = (message) => {
+    console.log(message.toUpperCase())
+}
+const greetingHandler = () => {
+    return {on: 'greeting', func:greeting}
+}
 
 const renderMessage = (message,source) => {
     console.log("#".repeat(10))
@@ -15,8 +22,24 @@ const renderMessage = (message,source) => {
     console.log("FROM FORM ID=> ", source)
     console.log("#".repeat(10))
 }
+
+const renderMessageHandler = () => {
+    return {on: 'inputMessage', func:renderMessage}
+}
+
+const handlers = [greetingHandler(), renderMessageHandler()]
+
+const socketServer = (httpServer, handlers) => {
+    const socket = new Server(httpServer)
+    socket.on('connection', (conn) =>{
+        for (const handler of handlers) {
+            conn.on(handler.on, handler.func)
+        }
+    })
+}
 export {
-    renderMessage,
+    socketServer,
+    handlers,
     __dirname,
     __public,
     __views,
