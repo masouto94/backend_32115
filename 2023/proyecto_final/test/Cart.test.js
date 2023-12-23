80bb4851ae1613ffcebb3614ca33f928734f083b
Matias Souto <masouto94@gmail.com>
2023-12-23 20:37:24 -0300

    all tests

--- 2023/proyecto_final/test/Cart.test.js
+++ 2023/proyecto_final/test/Cart.test.js
@@ -1,23 +1,40 @@
 import mongoose from "mongoose";
 import { cartModel } from "../src/model/Cart.js";
-import Assert, { strictEqual } from 'assert';
-import { expect } from "chai";
+import { userModel } from "../src/model/User.js";
+import { productModel } from "../src/model/Product.js";
+import { assert, expect } from "chai";
+import supertest from "supertest";
 
 await mongoose.connect(process.env.MONGO_URL)
+const requester = supertest('http://localhost:8080')
 
 describe('Cart tests', function () {
-    beforeEach(function(){
+    
+    beforeEach(() => {
         this.timeout(5000)
     })
-    describe('Cart schema validations', ()=>
-        it('Has all default keys', async function() {
-            const res = await cartModel.findOne()
-            
-            expect(Object.keys(res._doc)).to.include.members([
-                '_id',
-                'price',
-                'products'
-            ])
-        }
-     ))
+
+    it('Has all default keys', async function () {
+        const res = await cartModel.findOne()
+
+        expect(Object.keys(res._doc)).to.include.members([
+            '_id',
+            'price',
+            'products'
+        ])
+    })
+    it('Cart IDs are not orphan', async function () {
+        const cart_ids = (await cartModel.find({}, {_id: 1 })).map(c_id => c_id._id.toString())
+        const user_carts = (await userModel.find({}, { cart: 1 })).map(c_id => c_id.cart.toString())
+        expect(cart_ids).to.include.members(user_carts)
+    })
+    it('All products in carts exist', async function () {
+        const prods_in_carts = await cartModel.find({}, {products: 1 })
+        prods_in_carts.map(cart => {
+            cart.products.map(async (prod) => {
+                assert.isNotNull(await productModel.findById(prod.prod_id._id))
+            })
+        })
+    })
+    
 })
